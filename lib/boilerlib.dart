c744d6248b929080dcb1f397e6acf637c6ee5b01
faruq <kilo.dev@nexagroup.co.id>
2024-03-12 11:30:27 +0700

    add line code

--- lib/boilerlib.dart
+++ lib/boilerlib.dart
@@ -5,3 +5,4 @@ export 'core/camera/camera_widget.dart';
 export 'core/permission_util/permission_util.dart';
 export 'core/router/router_manager.dart';
 export 'core/storage_util/storage_util.dart';
+export 'package:go_router/go_router.dart';
